f41f3b4d10ceeb019dff016a2b4885716ce3d511
AnaLTFernandes <analuizateixeirafernandes184@gmal.com>
2022-06-10 11:35:38 -0300

    Começo da implantação de js

---
+++ script.js
@@ -0,0 +1,6 @@
+function escolher() {
+    let escolhido = document.querySelector(".opcao");
+    escolhido.classList.toggle("escolhido");
+    let icone = document.querySelector(".icone");
+    icone.classList.toggle("none");
+}
